21e98e7049689626be948ba62ab67747b3c60e9f
ru94jam <ph.mayer@campus.lmu.de>
2020-08-26 18:42:57 +0200

    intermediate state

--- src/TestParser.hs
+++ src/TestParser.hs
@@ -11,6 +11,7 @@ test2 = ["Wassermelone", "salamander"]
 wasser = ["4", "7"]
 
 --testParser = [html|<div class="test" id="{myId}">{{ myId }} hi geht heute {{ myId }}</div>|]
-testParser = [html|<div [a<-wasser]>Hello World</div>|]
+--testParser = [html|<div [a<-wasser]>Hello World</div>|]
+--testParser = [html|<div><span [a<-wasser]>{{test}} Hello World {{test}}</span></div>|]
 
 f a = a
