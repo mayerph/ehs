227724c4e25456191313b1e52d8e3fb285ec1211
ru94jam <ph.mayer@campus.lmu.de>
2020-09-05 13:25:38 +0200

    initData fix

--- src/Ehs.hs
+++ src/Ehs.hs
@@ -15,33 +15,33 @@ initData loc = do
     writeFile (loc ++ "Data.hs") templateData
 
 
-templateData = "{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}\
-\{-# Language FlexibleContexts #-}\
-\{-# LANGUAGE NoMonomorphismRestriction #-}\
-\{-# LANGUAGE OverloadedStrings #-}\
-\\
-\module Data where \
-\\
-\import Language.Haskell.TH \
-\import Language.Haskell.TH.Quote\
-\import Language.Haskell.TH.Syntax \
-\import Control.Monad\
-\import Control.Applicative\
-\\
-\data Template = \
-        \Empty\
-    \|   T_List [Template]\
-    \|   T_String String \
-    \|   T_Int Int \
-\ \
-    \deriving (Eq, Ord)\
-\\
-\instance Lift Template where\
-    \lift (T_String i) = appE (conE 'T_String) (lift i)\
-    \lift (T_Int i) = appE (conE 'T_Int) (lift i)\
-\\
-\instance Show Template where\
-    \show (Empty) = \"Empty\"\
-    \show (T_String i) = id i\
-    \show (T_Int i) = show i\
-    \show (T_List i) = show i"
+templateData = "{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}\n\
+\{-# Language FlexibleContexts #-}\n\
+\{-# LANGUAGE NoMonomorphismRestriction #-}\n\
+\{-# LANGUAGE OverloadedStrings #-}\n\
+\\n\
+\module Data where\n\
+\\n\
+\import Language.Haskell.TH\n\
+\import Language.Haskell.TH.Quote\n\
+\import Language.Haskell.TH.Syntax\n\
+\import Control.Monad\n\
+\import Control.Applicative\n\
+\\n\
+\data Template = \n\
+\\t    Empty\n\
+\\t|   T_List [Template]\n\
+\\t|   T_String String\n\
+\\t|   T_Int Int\n\
+\\n\
+\\tderiving (Eq, Ord)\n\
+\\n\
+\instance Lift Template where\n\
+\\tlift (T_String i) = appE (conE 'T_String) (lift i)\n\
+\\tlift (T_Int i) = appE (conE 'T_Int) (lift i)\n\
+\\n\
+\instance Show Template where\n\
+\\tshow (Empty) = \"Empty\"\n\
+\\tshow (T_String i) = id i\n\
+\\tshow (T_Int i) = show i\n\
+\\tshow (T_List i) = show i"
